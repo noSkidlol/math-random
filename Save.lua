17a4ca217fa801914c2daeaf94b93d5970ef504a
SupLuaYT <119862848+noSkidlol@users.noreply.github.com>
2023-01-03 12:16:38 +0800

    Create Save.lua

---
+++ Save.lua
@@ -0,0 +1,16 @@
+local Name = getgenv().SaveName
+local Description = {}
+if makefolder then
+    makefolder("Portal Hub")
+end
+
+local Settings;
+if not pcall(function() readfile("Portal Hub//" .. Name) end) then
+    writefile("Portal Hub//" .. Name, game:GetService("HttpService"):JSONEncode(Description))
+end
+
+Settings = game:GetService("HttpService"):JSONDecode(readfile("Portal Hub//" .. Name))
+
+local function Save()
+    writefile("Portal Hub//" .. Name, game:GetService("HttpService"):JSONEncode(Settings))
+end
