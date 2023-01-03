c57d457b4bef4394aa68f306a42e173cedae4cc5
SupLuaYT <119862848+noSkidlol@users.noreply.github.com>
2023-01-03 12:20:27 +0800

    Update and rename Save.lua to Script-Save.lua

--- Script-Save.lua
+++ Script-Save.lua
@@ -1,3 +1,4 @@
+local Lib = {}
 local Name = getgenv().SaveName
 local Description = {}
 if makefolder then
@@ -9,8 +10,8 @@ if not pcall(function() readfile("Portal Hub//" .. Name) end) then
     writefile("Portal Hub//" .. Name, game:GetService("HttpService"):JSONEncode(Description))
 end
 
-Settings = game:GetService("HttpService"):JSONDecode(readfile("Portal Hub//" .. Name))
-
-local function Save()
+Settings = game:GetService("HttpService"):JSONDecode(readfile("Portal Hub//" .. Name)
+function Lib:Save()
     writefile("Portal Hub//" .. Name, game:GetService("HttpService"):JSONEncode(Settings))
 end
+return Lib
